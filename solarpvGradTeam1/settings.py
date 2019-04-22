c685f86465fd4647421e85f8940c668b3dd2f9f6
jvalde13 <jvalde13@asu.edu>
2019-04-21 23:39:43 -0700

    fifth

--- solarpvGradTeam1/settings.py
+++ solarpvGradTeam1/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-#import mysql.connector
-#import pymysql
+import mysql.connector
 
-#pymysql.install_as_MySQLdb()
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -34,13 +34,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
     'SolarPV',
+
 ]
 
 MIDDLEWARE = [
@@ -81,12 +83,12 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'mysql.connector.django',
         'NAME': 'SolarPV',
         'USER': 'root',
-        'PASSWORD': 'rootroot',
-        #'HOST': '127.0.0.1',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
